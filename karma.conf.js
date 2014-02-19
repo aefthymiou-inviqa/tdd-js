a573054a2cb0e9b91ab89085909f13888af49182
Alistair Stead <alistair.stead@designdisclosure.com>
2014-02-19 22:55:44 +0000

    Moved original file structure

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'bower_components/chai/chai.js',
-      'public/js/**/*.js',
+      'js/**/*.js',
       'test/**/*.js'
     ],
 
