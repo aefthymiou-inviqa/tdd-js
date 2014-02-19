1cb50a7e7b38d944dfec0b3fa97696383d9934ad
Alistair Stead <alistair.stead@designdisclosure.com>
2014-02-19 23:24:39 +0000

    Angular unit test passing

--- test/angularSpec.js
+++ test/angularSpec.js
@@ -2,12 +2,17 @@ describe('Angular unit tests', function () {
 
   var should = chai.should();
   var element;
+  var $scope;
 
-  beforeEach(function () {
+  beforeEach(inject(function ($compile, $rootScope) {
+    $scope = $rootScope;
     element = angular.element('<div>{{2 + 2}}</div>');
-  });
+    element = $compile(element)($rootScope);
+  }));
 
   it('should return 4', function (done) {
+    $scope.$digest();
     element.html().should.equal('4');
+    done();
   });
 });
